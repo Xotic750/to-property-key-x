5fcc252c758e93a434c18dad50e6dd5f45f87643
Graham Fairweather <xotic750@gmail.com>
2019-07-31 21:48:52 +0200

    :bookmark: v3.0.19
    Auto build

--- dist/to-property-key-x.js
+++ dist/to-property-key-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-31T17:30:56.244Z",
+  "date": "2019-07-31T19:48:47.213Z",
   "describe": "",
   "description": "Converts argument to a value that can be used as a property key.",
   "file": "to-property-key-x.js",
